7718336a7a95bbf0f01619cd126ca9cd6e51fc0e
pkncoder <alex.h.preston@gmail.com>
2025-10-08 16:46:17 -0400

    Seperated the modals and the run command into different files in the helper module

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import gi
 
-from helpers.helpers import ModalWindow, runCommand
+from helpers.modals import ModalWindow, runCommand
 
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk, Gio  # noqa # type: ignore
